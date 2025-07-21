26b6996f379dbe81a86ee61448cafdba088e704b
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-07-21 18:50:37 -0400

    rotated thing

--- src/client/java/zedzee/github/io/buildableghasts/BuildableGhastsClient.java
+++ src/client/java/zedzee/github/io/buildableghasts/BuildableGhastsClient.java
@@ -2,52 +2,62 @@ package zedzee.github.io.buildableghasts;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.ShapeContext;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ColorHelper;
-import net.minecraft.util.math.Vec3d;
-import zedzee.github.io.buildableghasts.mixin.client.DrawBlockOutlineAccessor;
-
-import java.util.Random;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.math.*;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 
 public class BuildableGhastsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		WorldRenderEvents.BEFORE_BLOCK_OUTLINE.register((context, hitResult) -> {
-			DrawBlockOutlineAccessor outlineAccessor = ((DrawBlockOutlineAccessor) context.worldRenderer());
+//			if (!(hitResult instanceof EntityHitResult entityHitResult) || entityHitResult.getEntity().getType() != EntityType.HAPPY_GHAST) {
+//				return true;
+//			}
+			if (!(hitResult instanceof EntityHitResult entityHitResult)) {
+				return true;
+			}
+
+//			HappyGhastEntity happyGhast = (HappyGhastEntity) entityHitResult.getEntity();
+//
+//			// Has to be wearing the harness
+//			if (!happyGhast.isWearingBodyArmor()) {
+//				return true;
+//			}
 
 			MinecraftClient client = MinecraftClient.getInstance();
 			MatrixStack matrices = new MatrixStack();
 			BufferBuilderStorage bufferBuilders = client.getBufferBuilders();
 			VertexConsumer consumer = bufferBuilders.getEntityVertexConsumers().getBuffer(RenderLayer.getLines());
-			ClientPlayerEntity player = client.player;
-
 			Camera camera = context.camera();
 			Vec3d cameraPos = camera.getCameraPos();
 
-			if (!(hitResult instanceof BlockHitResult blockHitResult)) {
-				return false;
-			}
+			Entity entity = entityHitResult.getEntity();
+			Vec3d pos = entity.getPos();
 
-			BlockPos pos = blockHitResult.getBlockPos();
-			BlockState state = player.getWorld().getBlockState(pos);
+//			Box boundingBox = entity.getBoundingBox();
+//
+//			// get rid of the global position of the box
+//			boundingBox = boundingBox.offset(-pos.x, -pos.y, -pos.z);
+
+			VoxelShape shape = VoxelShapes.fullCube();
+			matrices.translate(pos.subtract(cameraPos));
+			matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-entity.getBodyYaw()));
 
 			VertexRendering.drawOutline(
 					matrices,
 					consumer,
-					state.getOutlineShape(player.getWorld(), pos, ShapeContext.of(player)),
-					((double)pos.getX() + 0.03f) - cameraPos.x,
-					((double)pos.getY() + 0.03f) - cameraPos.y,
-					(double)pos.getZ() - cameraPos.z,
+					shape,
+					-0.5,
+					-0.5,
+					-0.5,
 					ColorHelper.withAlpha(102, -16777216)
 			);
+			
 			return false;
 		});
 	}
