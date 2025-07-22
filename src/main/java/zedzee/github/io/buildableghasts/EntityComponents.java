b4ed915e8f05b55053948dd28a3213e21379ed87
zedzee <carsonuderitz@gmail.com>
2025-07-22 15:43:19 -0400

    Implemented the subgrid class & added CCA-entity

---
+++ src/main/java/zedzee/github/io/buildableghasts/EntityComponents.java
@@ -0,0 +1,11 @@
+package zedzee.github.io.buildableghasts;
+
+import org.ladysnake.cca.api.v3.entity.EntityComponentFactoryRegistry;
+import org.ladysnake.cca.api.v3.entity.EntityComponentInitializer;
+
+public class EntityComponents implements EntityComponentInitializer {
+    @Override
+    public void registerEntityComponentFactories(EntityComponentFactoryRegistry entityComponentFactoryRegistry) {
+
+    }
+}
--- src/main/java/zedzee/github/io/buildableghasts/grid/SubGrid.java
+++ src/main/java/zedzee/github/io/buildableghasts/grid/SubGrid.java
@@ -1,77 +1,90 @@
 package zedzee.github.io.buildableghasts.grid;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.component.type.NbtComponent;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
+import net.minecraft.nbt.NbtHelper;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.registry.RegistryEntryLookup;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SubGrid {
-    private final int width;
-    private final int height;
-    private final int depth;
-    private ArrayList<ArrayList<ArrayList<BlockState>>> blocks;
+    private int xSize;
+    private int ySize;
+    private int zSize;
+    private BlockState[] blocks;
+    private static final BlockState AIR = Blocks.AIR.getDefaultState();
 
     public SubGrid(int width, int height, int depth) {
-        this.width = width;
-        this.height = height;
-        this.depth = depth;
+        this.xSize = width;
+        this.ySize = height;
+        this.zSize = depth;
 
-        blocks = new ArrayList<>();
-        for (int i = 0; i < width; i++) {
-            ArrayList<ArrayList<BlockState>> row = new ArrayList<>();
-
-            for (int j = 0; j < height; j++) {
-                 ArrayList<BlockState> col = new ArrayList<>();
-
-                 for (int k = 0; k < depth; k++) {
-                     col.add(Blocks.AIR.getDefaultState());
-                 }
-
-                 row.add(col);
-            }
-            blocks.add(row);
-        }
+        blocks = new BlockState[width * height * depth];
+        Arrays.fill(blocks, AIR);
     }
 
-    public BlockState get(int x, int y, int z) {
-        return blocks.get(x).get(y).get(z);
-    }
-
-    public void set(int x, int y, int z, BlockState state) {
-        blocks.get(x).get(y).set(z, state);
-    }
-
-    public NbtCompound getNbt() {
+    public NbtCompound writeToNbt() {
         NbtCompound compound = new NbtCompound();
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                for (int k = 0; k < depth; k++) {
-                    BlockState state = get(i, j, k);
-                }
+        compound.putInt("x_size", xSize);
+        compound.putInt("y_size", ySize);
+        compound.putInt("z_size", zSize);
+
+        NbtList blockList = new NbtList();
+
+        for (int i = 0; i < blocks.length; i++) {
+            BlockState state = blocks[i];
+
+            if (state == null) {
+                continue;
             }
+
+            blockList.add(NbtHelper.fromBlockState(state));
         }
 
+        compound.put("blocks", blockList);
         return compound;
     }
 
-    public int getWidth() {
-        return width;
+    public void readFromNbt(RegistryEntryLookup<Block> blockRegistryLookup, NbtCompound compound) {
+        xSize = compound.getInt("x_size").get();
+        ySize = compound.getInt("y_size").get();
+        zSize = compound.getInt("z_size").get();
+
+        NbtList blocksList = compound.getList("blocks").get();
+        for (int i = 0; i < blocksList.size(); i++) {
+            blocks[i] = NbtHelper.toBlockState(blockRegistryLookup, blocksList.get(i).asCompound().get());
+        }
     }
 
-    public int getHeight() {
-        return height;
+    public void setAir(int i) {
+        blocks[i] = AIR;
     }
 
-    public int getDepth() {
-        return depth;
+    private int coordsToIdx(int x, int y, int z) {
+        return x + y * xSize + z * xSize * ySize;
     }
 
-    static record SubGridCell(Vec3i pos, NbtCompound nbtData, Block) {
+    public BlockState get(int x, int y, int z) {
+        return blocks[coordsToIdx(x, y, z)];
+    }
 
+    public void set(BlockState state, int x, int y, int z) {
+        blocks[coordsToIdx(x, y, z)] = state;
+    }
+
+    public int getXSize() {
+        return xSize;
+    }
+
+    public int getYSize() {
+        return ySize;
+    }
+
+    public int getZSize() {
+        return zSize;
     }
 }
