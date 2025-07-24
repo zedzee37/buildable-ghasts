6521e2e787edfbc1c0cfa0eba1aeb49d8445b264
zedzee <carsonuderitz@gmail.com>
2025-07-24 10:54:47 -0400

    g

---
+++ src/main/java/zedzee/github/io/buildableghasts/world/VirtualWorld.java
@@ -0,0 +1,170 @@
+package zedzee.github.io.buildableghasts.world;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.component.type.MapIdComponent;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.dragon.EnderDragonPart;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.FuelRegistry;
+import net.minecraft.item.map.MapState;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.recipe.BrewingRecipeRegistry;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.resource.featuretoggle.FeatureSet;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.ChunkManager;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.entity.EntityLookup;
+import net.minecraft.world.event.GameEvent;
+import net.minecraft.world.explosion.ExplosionBehavior;
+import net.minecraft.world.tick.QueryableTickScheduler;
+import net.minecraft.world.tick.TickManager;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+
+public class VirtualWorld extends World {
+    protected VirtualWorld(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates) {
+        super(properties, registryRef, registryManager, dimensionEntry, isClient, debugWorld, seed, maxChainedNeighborUpdates);
+    }
+
+    @Override
+    public void updateListeners(BlockPos pos, BlockState oldState, BlockState newState, int flags) {
+
+    }
+
+    @Override
+    public void playSound(@Nullable Entity source, double x, double y, double z, RegistryEntry<SoundEvent> sound, SoundCategory category, float volume, float pitch, long seed) {
+
+    }
+
+    @Override
+    public void playSoundFromEntity(@Nullable Entity source, Entity entity, RegistryEntry<SoundEvent> sound, SoundCategory category, float volume, float pitch, long seed) {
+
+    }
+
+    @Override
+    public void createExplosion(@Nullable Entity entity, @Nullable DamageSource damageSource, @Nullable ExplosionBehavior behavior, double x, double y, double z, float power, boolean createFire, ExplosionSourceType explosionSourceType, ParticleEffect smallParticle, ParticleEffect largeParticle, RegistryEntry<SoundEvent> soundEvent) {
+
+    }
+
+    @Override
+    public String asString() {
+        return "";
+    }
+
+    @Override
+    public @Nullable Entity getEntityById(int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<EnderDragonPart> getEnderDragonParts() {
+        return List.of();
+    }
+
+    @Override
+    public TickManager getTickManager() {
+        return null;
+    }
+
+    @Override
+    public @Nullable MapState getMapState(MapIdComponent id) {
+        return null;
+    }
+
+    @Override
+    public void setBlockBreakingInfo(int entityId, BlockPos pos, int progress) {
+
+    }
+
+    @Override
+    public Scoreboard getScoreboard() {
+        return null;
+    }
+
+    @Override
+    public RecipeManager getRecipeManager() {
+        return null;
+    }
+
+    @Override
+    protected EntityLookup<Entity> getEntityLookup() {
+        return null;
+    }
+
+    @Override
+    public BrewingRecipeRegistry getBrewingRecipeRegistry() {
+        return null;
+    }
+
+    @Override
+    public FuelRegistry getFuelRegistry() {
+        return null;
+    }
+
+    @Override
+    public ChunkManager getChunkManager() {
+        return null;
+    }
+
+    @Override
+    public void syncWorldEvent(@Nullable Entity source, int eventId, BlockPos pos, int data) {
+
+    }
+
+    @Override
+    public void emitGameEvent(RegistryEntry<GameEvent> event, Vec3d emitterPos, GameEvent.Emitter emitter) {
+
+    }
+
+    @Override
+    public float getBrightness(Direction direction, boolean shaded) {
+        return 0;
+    }
+
+    @Override
+    public List<? extends PlayerEntity> getPlayers() {
+        return List.of();
+    }
+
+    @Override
+    public RegistryEntry<Biome> getGeneratorStoredBiome(int biomeX, int biomeY, int biomeZ) {
+        return null;
+    }
+
+    @Override
+    public int getSeaLevel() {
+        return 0;
+    }
+
+    @Override
+    public FeatureSet getEnabledFeatures() {
+        return null;
+    }
+
+    @Override
+    public QueryableTickScheduler<Block> getBlockTickScheduler() {
+        return super.getBlockTickScheduler();
+    }
+
+    @Override
+    public QueryableTickScheduler<Fluid> getFluidTickScheduler() {
+        return null;
+    }
+}
